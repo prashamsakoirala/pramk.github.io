87fdcc26d64c98e2001970336b25ea07be12e566
prashamsakoirala <122059885+prashamsakoirala@users.noreply.github.com>
2023-08-06 21:52:14 -0400

    filesupload

---
+++ site.js
@@ -0,0 +1,42 @@
+window.onload = function() {
+    document.getElementById('top').scrollIntoView()
+};
+window.onscroll = function() {
+    myFunction()
+};
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var fadeText = document.querySelector(".load-screen");
+  
+    fadeText.classList.add("fade-in");
+  
+    fadeText.addEventListener("animationend", function() {
+      fadeText.classList.remove("fade-in");
+    });
+  });
+  
+
+
+function myFunction() {
+  if (document.documentElement.scrollTop > 750) {
+    document.getElementById("top").style.backgroundColor = "#FCF7E5";
+    updateNavLinksColor(true);
+
+  } else {
+    document.getElementById("top").style.backgroundColor = "#9FB8AD";
+    updateNavLinksColor(false);
+  }
+}
+
+function updateNavLinksColor(isScrollPast750) {
+    const navLinks = document.querySelectorAll("nav a");
+    navLinks.forEach(link => {
+        if (isScrollPast750) {
+            link.classList.add("scroll-past-750");
+        } else {
+            link.classList.remove("scroll-past-750");
+        }
+    });
+}
